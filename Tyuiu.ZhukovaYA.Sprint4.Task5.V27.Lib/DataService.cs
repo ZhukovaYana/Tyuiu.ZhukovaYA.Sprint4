fa1a70c81b522051bdc4f81e4ef566c8b390c5fc
Yana <yana_zhukova2004@mail.ru>
2024-11-09 17:26:22 +0500

    Task5

---
+++ Tyuiu.ZhukovaYA.Sprint4.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZhukovaYA.Sprint4.Task5.V27.Lib
+{
+    public class DataService : ISprint4Task5V27
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int res = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (matrix[i, j] < 0) res ++;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZhukovaYA.Sprint4.Task5.V27/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Tyuiu.ZhukovaYA.Sprint4.Task5.V27.Lib;
+namespace Tyuiu.ZhukovaYA.Sprint4.Task5.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Жукова Яна Андреевна | ИИПБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Двумерные массивы (генератор случайных чисел)                           *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Жукова Яна Андреевна | ИИПБ-24-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дан двумерный целочисленный массив 5 на 5 элементов, заполненный случайны*");
+            Console.WriteLine("*ыми значениями в диапазоне от -5 до 7. Найти количество отрицательных эле*");
+            Console.WriteLine("*ементов.                                                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Random random = new Random();
+            int[,] array = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i,j] = random.Next(-5, 7);
+                    Console.WriteLine($"Элемент массива [{i},{j}]: {array[i,j]}");
+                }
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine($"кол-во отрицательных элементов: {ds.Calculate(array)}");
+            Console.ReadKey();
+
+        }
+    }
+}

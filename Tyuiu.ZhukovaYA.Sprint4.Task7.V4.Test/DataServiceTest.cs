d323c4b5704eddeed59742ca84deb98c0266c522
Yana <yana_zhukova2004@mail.ru>
2024-11-09 18:08:55 +0500

    Task7

---
+++ Tyuiu.ZhukovaYA.Sprint4.Task7.V4.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ZhukovaYA.Sprint4.Task7.V4.Lib;
+
+namespace Tyuiu.ZhukovaYA.Sprint4.Task7.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(8, ds.Calculate(2, 2, "3456"));
+        }
+    }
+}
---
+++ Tyuiu.ZhukovaYA.Sprint4.Task7.V4/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.ZhukovaYA.Sprint4.Task7.V4.Lib;
+namespace Tyuiu.ZhukovaYA.Sprint4.Task7.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Жукова Яна Андреевна | ИИПБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Жукова Яна Андреевна | ИИПБ-24-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дана строка из одноразрядных цифр \"382976421897\". Преобразуйте ее в матри*");
+            Console.WriteLine("*ицу 3 на 4 и подсчитайте сумму нечетных чисел.                           *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int n = 3;
+            int m = 4;
+            string str = "382976421897";
+            int index = 0;
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine($"сумма нечетных чисел: {ds.Calculate(n,m,str)}");
+            Console.ReadKey();
+        }
+    }
+}
